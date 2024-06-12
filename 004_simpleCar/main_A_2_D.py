900e5b47d481c8e41194d10fb9af1bcbee15c01c
kamakiri1225 <36812492+kamakiri1225@users.noreply.github.com>
2024-06-12 23:27:51 +0900

    Add files via upload
    002_full_car0000_0001.rad修正

---
+++ 004_simpleCar/main_A_2_D.py
@@ -0,0 +1,7 @@
+# Load the module
+from vortex_radioss.animtod3plot.Anim_to_D3plot import readAndConvert
+  
+# Use the file stem e.g for "folder/fileA001", "folder/fileA002" would be:
+readAndConvert("/mnt/c/work/001_CAE/openradioss/work/20240611_simpleCar/004_simpleCar_blog/002_full_car0000")
+
+# The D3plots files "folder/file.d3plot*" will be generated
